06b3670e9541c77557fa63a2c1a623d35c2a9803
Surya <gayashiva91@gmail.com>
2020-11-04 17:05:48 +0530

    added coordinates

--- ECMWF_query.py
+++ ECMWF_query.py
@@ -3,147 +3,195 @@ import pprint
 
 
 class ECMWF_query:
+    def __init__(self):
 
-	def __init__(self):
+        # https://www.ecmwf.int/en/forecasts/access-forecasts/ecmwf-web-api
+        self.use_era5 = True
+        self.start_year = 2019
+        self.end_year = 2020
+        self.resultsdir = "results/"
+        self.debug = False
+        self.time_units = "days since 1948-01-01 00:00:00"
+        self.optionals = False  # optional variables to extract depending on ROMS version (Rutgers or Kate)
 
-		# https://www.ecmwf.int/en/forecasts/access-forecasts/ecmwf-web-api
-		self.use_era5 = True
-		self.start_year = 1980
-		self.end_year = 1981
-		self.resultsdir = "results/"
-		self.debug = False
-		self.time_units = "days since 1948-01-01 00:00:00"
-		self.optionals = False  # optional variables to extract depending on ROMS version (Rutgers or Kate)
+        if not os.path.exists(self.resultsdir):
+            os.mkdir(self.resultsdir)
+        if self.use_era5:
+            self.dataset = "era5"
+            self.dataset_class = "ea"
+            self.grid = "0.25/0.25"
+        else:
+            self.dataset = "interim"
+            self.dataset_class = "ei"
+            self.grid = "0.75/0.75"
 
-		if not os.path.exists(self.resultsdir):
-			os.mkdir(self.resultsdir)
-		if self.use_era5:
-			self.dataset = 'era5'
-			self.dataset_class = 'ea'
-			self.grid = '0.25/0.25'
-		else:
-			self.dataset = 'interim'
-			self.dataset_class = 'ei'
-			self.grid = '0.75/0.75'
+        self.reanalysis = "reanalysis-era5-single-levels"  # 'reanalysis-era5-complete'
+        # self.area = "60/0/59/1"  # test setup North/West/South/East
+        self.site = "schwarzsee"
 
-		self.reanalysis = 'reanalysis-era5-single-levels'  # 'reanalysis-era5-complete'
-		#self.area = "60/0/59/1"  # test setup North/West/South/East
-		self.area = "90/-180/44/180"
+        if self.site == "schwarzsee":
+            self.area = "46.7/7.24/46.64/7.3"
+        if self.site == "leh":
+            self.area = "34.11/77.45/34.09/77.55"
+        if self.site == "guttannen":
+            self.area = "46.7/8.25/46.64/8.31"
+        self.resultsdir += self.site
 
-		self.parameters = ['10m_u_component_of_wind',
-						   '10m_v_component_of_wind',
-						   '2m_temperature',
-						   'mean_sea_level_pressure',
-						   'mean_surface_downward_long_wave_radiation_flux',
-						   'mean_surface_downward_short_wave_radiation_flux',
-						   'total_cloud_cover',
-						   'total_precipitation',
-						   'specific_humidity']
+        self.parameters = [
+            "10m_u_component_of_wind",
+            "10m_v_component_of_wind",
+            "2m_temperature",
+            "2m_dewpoint_temperature",
+            "surface_pressure",
+            "surface_solar_radiation_downwards",
+            "surface_thermal_radiation_downwards",
+            "total_sky_direct_solar_radiation_at_surface",
+            "total_cloud_cover",
+            "total_precipitation",
+            "specific_humidity",
+        ]
 
-		if self.optionals:
-			self.parameters.append('evaporation',
-								   'mean_surface_downward_short_wave_radiation_flux',
-								   'mean_surface_net_short_wave_radiation_flux'
-								   'mean_surface_sensible_heat_flux',
-								   'mean_surface_latent_heat_flux',
-								   'mean_surface_net_long_wave_radiation_flux',)
+        if self.optionals:
+            self.parameters.append(
+                "evaporation",
+                "mean_surface_downward_short_wave_radiation_flux",
+                "mean_surface_net_short_wave_radiation_flux"
+                "mean_surface_sensible_heat_flux",
+                "mean_surface_latent_heat_flux",
+                "mean_surface_net_long_wave_radiation_flux",
+            )
 
-	def info(self):
-		pprint("ERA5: \n Reanalysis: 0.25°x0.25° (atmosphere), 0.5°x0.5° (ocean waves) \n \
+    def info(self):
+        pprint(
+            "ERA5: \n Reanalysis: 0.25°x0.25° (atmosphere), 0.5°x0.5° (ocean waves) \n \
 		Period: 1979 - present \n \
 		More info on ERA5 can be found here:\n \
-		https://cds.climate.copernicus.eu/cdsapp#!/dataset/reanalysis-era5-single-levels-monthly-means?tab=overview")
+		https://cds.climate.copernicus.eu/cdsapp#!/dataset/reanalysis-era5-single-levels-monthly-means?tab=overview"
+        )
 
-	# List of parameters to download:
-	# https://apps.ecmwf.int/codes/grib/param-db
-	# https: // apps.ecmwf.int / data - catalogues / era5 / batch / 3647799 /
+    # List of parameters to download:
+    # https://apps.ecmwf.int/codes/grib/param-db
+    # https: // apps.ecmwf.int / data - catalogues / era5 / batch / 3647799 /
 
-	def get_parameter_metadata(self, parameter):
-		return {'2m_temperature': {'parameter_id': '167.128',
-								   'short_name': 't2m',
-								   'roms_name': 'Tair',
-								   'name': '2 metre temperature',
-								   'units': 'K',
-								   'time_name': 'Tair_time'},
-				'2m_dewpoint_temperature': {'parameter_id': '168.128',
-											'short_name': 'd2m',
-											'roms_name': 'Qair',
-											'name': '2 metre dewpoint temperature',
-											'units': 'K',
-											'time_name': 'tdew_time'},
-				'specific_humidity': {'parameter_id': '133.128',
-									  'short_name': 'q',
-									  'roms_name': 'Qair',
-									  'name': 'specific humidity',
-									  'units': 'kg kg-1',
-									  'time_name': 'qair_time'},
-				'10m_v_component_of_wind': {'parameter_id': '166.128',
-											'short_name': 'v10',
-											'roms_name': 'Vwind',
-											'name': '10 metre v-wind component',
-											'units': 'm s-1',
-											'time_name': 'wind_time'},
-				'10m_u_component_of_wind': {'parameter_id': '165.128',
-											'short_name': 'u10',
-											'roms_name': 'Uwind',
-											'name': '10 metre u-wind component',
-											'units': 'm s-1',
-											'time_name': 'wind_time'},
-				'mean_sea_level_pressure': {'parameter_id': '151.128',
-											'short_name': 'msl',
-											'roms_name': 'Pair',
-											'name': 'Mean sea level pressure',
-											'units': 'Pa',
-											'time_name': 'pair_time'},
-				'total_cloud_cover': {'parameter_id': '164.128',
-									  'short_name': 'tcc',
-									  'roms_name': 'cloud',
-									  'name': 'Total cloud cover',
-									  'units': '(0-1)',
-									  'time_name': 'cloud_time'},
-				'total_precipitation': {'parameter_id': '228.128',
-										'short_name': 'tp',
-										'roms_name': 'rain',
-										'name': 'Total precipitation',
-										'units': 'm',
-										'time_name': 'rain_time'},
-				'mean_surface_net_short_wave_radiation_flux': {'parameter_id': '37.235',
-															   'short_name': 'msnswrf',
-															   'roms_name': 'swrad',
-															   'name': 'Mean surface net short-wave radiation flux',
-															   'units': 'W m-2'},
-				'mean_surface_net_long_wave_radiation_flux': {'parameter_id': '38.235',
-															  'short_name': 'msnlwrf',
-															  'roms_name': 'lwrad',
-															  'name': 'Mean surface net long-wave radiation flux',
-															  'units': 'W m-2',
-															  'time_name': 'swrad_time'},
-				'mean_surface_downward_long_wave_radiation_flux': {'parameter_id': '36.235',
-																   'short_name': 'msdwlwrf',
-																   'roms_name': 'lwrad_down',
-																   'name': 'Mean surface downward long-wave radiation flux',
-																   'units': 'W m-2',
-																   'time_name': 'lwrad_time'},
-				'mean_surface_latent_heat_flux': {'parameter_id': '34.235',
-												  'short_name': 'mslhf',
-												  'roms_name': 'latent',
-												  'name': 'Surface latent heat flux',
-												  'units': 'W m-2',
-												  'time_name': 'swrad_time'},
-				'mean_surface_sensible_heat_flux': {'parameter_id': '33.235',
-													'short_name': 'msshf',
-													'roms_name': 'sensible',
-													'name': 'Surface sensible heat flux', 'units': 'W m-2',
-													'time_name': 'sen_time'},
-				'evaporation': {'parameter_id': '182.128',
-								'short_name': 'e',
-								'roms_name': 'evaporation',
-								'name': 'Evaporation',
-								'units': 'm of water equivalent',
-								'time_name': 'rain_time'},
-				'mean_surface_downward_short_wave_radiation_flux': {'parameter_id': 'None',
-																	'short_name': 'msdwswrf',
-																	'roms_name': 'swrad',
-																	'name': 'Mean surface downward short-wave radiation flux',
-																	'units': 'W m-2',
-																	'time_name': 'swrad_time'}}[parameter]
+    def get_parameter_metadata(self, parameter):
+        return {
+            "2m_temperature": {
+                "parameter_id": "167.128",
+                "short_name": "t2m",
+                "roms_name": "Tair",
+                "name": "2 metre temperature",
+                "units": "K",
+                "time_name": "Tair_time",
+            },
+            "2m_dewpoint_temperature": {
+                "parameter_id": "168.128",
+                "short_name": "d2m",
+                "roms_name": "Qair",
+                "name": "2 metre dewpoint temperature",
+                "units": "K",
+                "time_name": "tdew_time",
+            },
+            "specific_humidity": {
+                "parameter_id": "133.128",
+                "short_name": "q",
+                "roms_name": "Qair",
+                "name": "specific humidity",
+                "units": "kg kg-1",
+                "time_name": "qair_time",
+            },
+            "10m_v_component_of_wind": {
+                "parameter_id": "166.128",
+                "short_name": "v10",
+                "roms_name": "Vwind",
+                "name": "10 metre v-wind component",
+                "units": "m s-1",
+                "time_name": "wind_time",
+            },
+            "10m_u_component_of_wind": {
+                "parameter_id": "165.128",
+                "short_name": "u10",
+                "roms_name": "Uwind",
+                "name": "10 metre u-wind component",
+                "units": "m s-1",
+                "time_name": "wind_time",
+            },
+            "mean_sea_level_pressure": {
+                "parameter_id": "151.128",
+                "short_name": "msl",
+                "roms_name": "Pair",
+                "name": "Mean sea level pressure",
+                "units": "Pa",
+                "time_name": "pair_time",
+            },
+            "total_cloud_cover": {
+                "parameter_id": "164.128",
+                "short_name": "tcc",
+                "roms_name": "cloud",
+                "name": "Total cloud cover",
+                "units": "(0-1)",
+                "time_name": "cloud_time",
+            },
+            "total_precipitation": {
+                "parameter_id": "228.128",
+                "short_name": "tp",
+                "roms_name": "rain",
+                "name": "Total precipitation",
+                "units": "m",
+                "time_name": "rain_time",
+            },
+            "mean_surface_net_short_wave_radiation_flux": {
+                "parameter_id": "37.235",
+                "short_name": "msnswrf",
+                "roms_name": "swrad",
+                "name": "Mean surface net short-wave radiation flux",
+                "units": "W m-2",
+            },
+            "mean_surface_net_long_wave_radiation_flux": {
+                "parameter_id": "38.235",
+                "short_name": "msnlwrf",
+                "roms_name": "lwrad",
+                "name": "Mean surface net long-wave radiation flux",
+                "units": "W m-2",
+                "time_name": "swrad_time",
+            },
+            "mean_surface_downward_long_wave_radiation_flux": {
+                "parameter_id": "36.235",
+                "short_name": "msdwlwrf",
+                "roms_name": "lwrad_down",
+                "name": "Mean surface downward long-wave radiation flux",
+                "units": "W m-2",
+                "time_name": "lwrad_time",
+            },
+            "mean_surface_latent_heat_flux": {
+                "parameter_id": "34.235",
+                "short_name": "mslhf",
+                "roms_name": "latent",
+                "name": "Surface latent heat flux",
+                "units": "W m-2",
+                "time_name": "swrad_time",
+            },
+            "mean_surface_sensible_heat_flux": {
+                "parameter_id": "33.235",
+                "short_name": "msshf",
+                "roms_name": "sensible",
+                "name": "Surface sensible heat flux",
+                "units": "W m-2",
+                "time_name": "sen_time",
+            },
+            "evaporation": {
+                "parameter_id": "182.128",
+                "short_name": "e",
+                "roms_name": "evaporation",
+                "name": "Evaporation",
+                "units": "m of water equivalent",
+                "time_name": "rain_time",
+            },
+            "mean_surface_downward_short_wave_radiation_flux": {
+                "parameter_id": "None",
+                "short_name": "msdwswrf",
+                "roms_name": "swrad",
+                "name": "Mean surface downward short-wave radiation flux",
+                "units": "W m-2",
+                "time_name": "swrad_time",
+            },
+        }[parameter]
--- README.md
+++ README.md
@@ -8,7 +8,8 @@
 After conversations with ECMWF support I changed the code to only use ERA5 parameter names and not parameter IDs as these did not
 result in stable downloads (some parameter IDs seems to be missing for some months while the parameter names are consistent).
 
-This toolbox enables you to download ERA5 atmospheric forcing data for your model domain for a specified period.  The toolbox uses the [*Climate Data Store*] Python API to connect and download specific variables required by ROMS to perform simulations with atmospheric forcing. These variables are included in the list below:
+This toolbox enables you to download ERA5 atmospheric forcing data for your model domain for a specified period. The toolbox uses the [*Climate Data Store*] Python API to connect and download specific variables required by ROMS to perform simulations with atmospheric forcing. These variables are included in the list below:
+
 ```
    'Specific_humidity',
    '10m_u_component_of_wind',
@@ -26,17 +27,21 @@ This toolbox enables you to download ERA5 atmospheric forcing data for your mode
    'Evaporation',
    'Mean surface downward short-wave radiation flux'
 ```
+
 Details of the ERA5 variables can be found [here](https://cds.climate.copernicus.eu/cdsapp#!/dataset/reanalysis-era5-single-levels?tab=overview)
 To see the details for how ROMS requires naming convention etc.
-#### *Install API*
+
+#### _Install API_
+
 To start signup and get necessary credentials at the [*Climate Data Store*]. Store the credentials in a file called `.cdsapirc`in you root `$HOME` directory. It should look something like this:
 
 url: https://cds.climate.copernicus.eu/api/v2
 key: 28122:f85a4564-8895-498d-ad8a-gf274ba38d2r
 ls -lrt
-#### *Edit the toolbox settings*
-Edit the file `ECMWF_query.py`to define the start and end period you want to download data. If you want you can edit the months, days, and time steps of the day data will be downloaded in the file `ECMWF_tools.py` but by default the program downloads data for all available reanalysis time steps of the day for all days for all months of the year. Each result file contains data for one variable for one year.
 
+#### _Edit the toolbox settings_
+
+Edit the file `ECMWF_query.py`to define the start and end period you want to download data. If you want you can edit the months, days, and time steps of the day data will be downloaded in the file `ECMWF_tools.py` but by default the program downloads data for all available reanalysis time steps of the day for all days for all months of the year. Each result file contains data for one variable for one year.
 
 **Note**: If you are using Kate Hedstroms ROMS version with sea-ice you need to download 'mean_surface_downward_short_wave_radiation_flux' while the
 regular Rutgers ROMS version uses 'mean_surface_net_short_wave_radiation_flux'.
@@ -48,11 +53,13 @@ Data availability from Copernicus available here [https://cp-availability.ceda.a
 
 The region for where you extract the data is defined by the variable `self.area = "80/0/50/25"`found in `ECMWF_query.py`. The area is constrained by `North/West/South/East`.
 
-The time units in the  resulting ROMS files are converted from the ERA5 units (`1900-01-01`) to the standard ROMS reference time `1948-01-01`.
+The time units in the resulting ROMS files are converted from the ERA5 units (`1900-01-01`) to the standard ROMS reference time `1948-01-01`.
 The toolbox uses the netCDF4 `date2num`and `num2date` functions for this conversion.
 
-#### *Main query*
+#### _Main query_
+
 The main query for the call for data is found in ECMWF_tools.py
+
 ```Python
 		def submit_request(self, parameter, year, out_filename):
 
@@ -110,16 +117,18 @@ The main query for the call for data is found in ECMWF_tools.py
 
 ```
 
-####*Run the toolbox*
+####_Run the toolbox_
 To run the toolbox after editing the settings simply run
 `python ECMWF_tools.py`
 
 ### Global data
+
 If your model domain covers the entire Arctic, or northern hemisphere and reaches from -180 to 180 you have to convert
 the ERA5 data to be connected. This is done by introducing a fake point at 180 which is identical to point 0. This
 makes the data across the meridian line con`sistent and ROMS won`t choke on it. This step requires CDO installed.
 
 Run `make_files_fully_global-sh`:
+
 ```Bash
 for filename in results/*.nc; do
 		echo "Converting: " "$filename" " to" "halo/$(basename "$filename" .nc)_halo.nc"
@@ -127,10 +136,10 @@ for filename in results/*.nc; do
 done
 ```
 
-####*Unittest*
+####_Unittest_
 A few simple unittests are included in `test_ERA5.py`.
 
 [1]: https://buildkite.com/rask-dev-llc/era5-toolbox
-[image-1]:	https://badge.buildkite.com/9fe63ac4afc901fb503d10d67c26175d7071137729c00d1b17.svg
-[image-2]:	https://codebeat.co/badges/402a5755-c757-4a8d-a9a5-f9349aed8462
-[image-3]:	https://codecov.io/gh/trondkr/ERA5-ROMS/branch/master/graph/badge.svg
+[image-1]: https://badge.buildkite.com/9fe63ac4afc901fb503d10d67c26175d7071137729c00d1b17.svg
+[image-2]: https://codebeat.co/badges/402a5755-c757-4a8d-a9a5-f9349aed8462
+[image-3]: https://codecov.io/gh/trondkr/ERA5-ROMS/branch/master/graph/badge.svg
